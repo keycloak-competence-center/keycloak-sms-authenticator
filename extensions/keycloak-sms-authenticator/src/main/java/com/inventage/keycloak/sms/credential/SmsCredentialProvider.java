9c885234099809ebc025a6955bfcbc41c677db74
David Bidjan <david.bidjan@inventage.com>
2024-08-30 11:29:27 +0200

    Before adding the same phone number check if it has already been configured.

--- extensions/keycloak-sms-authenticator/src/main/java/com/inventage/keycloak/sms/credential/SmsCredentialProvider.java
+++ extensions/keycloak-sms-authenticator/src/main/java/com/inventage/keycloak/sms/credential/SmsCredentialProvider.java
@@ -1,7 +1,8 @@
 package com.inventage.keycloak.sms.credential;
 
-import com.inventage.keycloak.sms.models.credential.SmsCredentialModel;
 import com.inventage.keycloak.sms.authentication.requireactions.SmsRequiredAction;
+import com.inventage.keycloak.sms.models.credential.SmsCredentialModel;
+import com.inventage.keycloak.sms.models.credential.dto.SmsCredentialData;
 import org.jboss.logging.Logger;
 import org.keycloak.common.util.Time;
 import org.keycloak.credential.CredentialModel;
@@ -11,6 +12,11 @@ import org.keycloak.credential.CredentialTypeMetadataContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
+import org.keycloak.util.JsonSerialization;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
 
 public class SmsCredentialProvider implements CredentialProvider<SmsCredentialModel> {
 
@@ -29,15 +35,42 @@ public class SmsCredentialProvider implements CredentialProvider<SmsCredentialMo
             credentialModel.setCreatedDate(Time.currentTimeMillis());
         }
 
-        return allowMultipleCredentials() ? addCredential(user, credentialModel) : updateCredential(realm, user, credentialModel);
+        return allowMultipleCredentials() ? addCredential(realm, user, credentialModel) : updateCredential(realm, user, credentialModel);
     }
 
     private boolean allowMultipleCredentials() {
         return true;
     }
 
-    private CredentialModel addCredential(UserModel user, SmsCredentialModel credentialModel) {
+    private CredentialModel addCredential(RealmModel realm, UserModel user, SmsCredentialModel credentialModel) {
+        if (samePhoneNumberAlreadyConfigured(user, credentialModel)) {
+            return updateCredential(realm, user, credentialModel);
+        }
+
         return user.credentialManager().createStoredCredential(credentialModel);
+    }
+
+    private boolean samePhoneNumberAlreadyConfigured(UserModel user, SmsCredentialModel credentialModel) {
+        Optional<CredentialModel> smsCredentialModel = getCredentialModelWithSamePhoneNumber(user, credentialModel);
+
+        return smsCredentialModel.isPresent();
+    }
+
+    private Optional<CredentialModel> getCredentialModelWithSamePhoneNumber(UserModel user, SmsCredentialModel credentialModel) {
+        return user.credentialManager().getStoredCredentialsByTypeStream(getType())
+            .filter
+                (
+                    cred -> {
+                        final SmsCredentialData smsCredentialData;
+                        try {
+                            smsCredentialData = JsonSerialization.readValue(cred.getCredentialData(), SmsCredentialData.class);
+                            return Objects.equals(smsCredentialData.getPhoneNumber(), credentialModel.getSmsCredentialData().getPhoneNumber());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                )
+            .findFirst();
     }
 
     // replace the sms credential if there is already one
@@ -47,9 +80,8 @@ public class SmsCredentialProvider implements CredentialProvider<SmsCredentialMo
             credentialModel.setId(smsCredentialModel.getId());
             user.credentialManager().updateStoredCredential(credentialModel);
             return credentialModel;
-        }
-        else {
-            return addCredential(user, credentialModel);
+        } else {
+            return addCredential(realm, user, credentialModel);
         }
     }
 
