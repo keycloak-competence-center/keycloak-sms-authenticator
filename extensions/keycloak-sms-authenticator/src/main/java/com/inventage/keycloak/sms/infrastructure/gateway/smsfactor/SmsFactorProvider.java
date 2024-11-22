d7061db284da0c3c846b19b4e5d954bf5e9710f5
David Bidjan <david.bidjan@inventage.com>
2024-11-22 12:57:10 +0100

    GH-9: Add possibility to configure SMS Factor Gateway via environment variables

--- extensions/keycloak-sms-authenticator/src/main/java/com/inventage/keycloak/sms/infrastructure/gateway/smsfactor/SmsFactorProvider.java
+++ extensions/keycloak-sms-authenticator/src/main/java/com/inventage/keycloak/sms/infrastructure/gateway/smsfactor/SmsFactorProvider.java
@@ -7,6 +7,7 @@ import com.inventage.keycloak.sms.infrastructure.gateway.smsfactor.acl.SmsFactor
 import org.keycloak.component.ComponentModel;
 import org.keycloak.connections.httpclient.HttpClientProvider;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.utils.StringUtil;
 
 import java.util.Optional;
 
@@ -14,16 +15,21 @@ import java.util.Optional;
  * {@inheritDoc}
  *
  * {@link SmsService} provider for <a href="https://www.smsfactor.com/">SMS Factor</a>.
+ * The Service is configurable via components or environmental variables.
+ * If in both ways configured, then configuration via components get precedence.
  */
 public class SmsFactorProvider implements SmsServiceProvider {
 
-    /* package private */ static final String SMS_FACTOR_SERVICE_URL_DEFAULT = "https://api.smsfactor.com/send";
     /* package private */ static final String SMS_FACTOR_SERVICE_URL_CONFIG = "service-url";
+    /* package private */ static final String SMS_FACTOR_SERVICE_URL_ENV = "SMS_FACTOR_SERVICE_URL";
+    /* package private */ static final String SMS_FACTOR_SERVICE_URL_DEFAULT = "https://api.smsfactor.com/send";
 
     /* package private */ static final String SMS_FACTOR_SENDER_ADDRESS_CONFIG = "sender-address";
+    /* package private */ static final String SMS_FACTOR_SENDER_ADDRESS_ENV = "SMS_FACTOR_SENDER_ADDRESS";
     /* package private */ static final String SMS_FACTOR_SENDER_ADDRESS_DEFAULT = "Keycloak";
 
     /* package private */ static final String SMS_FACTOR_TOKEN_CONFIG = "sms-factor-token";
+    /* package private */ static final String SMS_FACTOR_TOKEN_ENV = "SMS_FACTOR_TOKEN";
     /* package private */ static final String SMS_FACTOR_TOKEN_DEFAULT = "token";
 
     private final KeycloakSession session;
@@ -56,6 +62,12 @@ public class SmsFactorProvider implements SmsServiceProvider {
         if (componentModel.isPresent()) {
             return componentModel.get().get(SMS_FACTOR_SENDER_ADDRESS_CONFIG);
         }
+
+        final String senderAddress = System.getenv(SMS_FACTOR_SENDER_ADDRESS_ENV);
+        if (StringUtil.isNotBlank(senderAddress)) {
+            return senderAddress;
+        }
+
         return SMS_FACTOR_SENDER_ADDRESS_DEFAULT;
     }
 
@@ -70,14 +82,26 @@ public class SmsFactorProvider implements SmsServiceProvider {
             return componentModel.get().get(SMS_FACTOR_TOKEN_CONFIG);
         }
 
+        final String baseUrl = System.getenv(SMS_FACTOR_TOKEN_ENV);
+        if (StringUtil.isNotBlank(baseUrl)) {
+            return baseUrl;
+        }
+
         return SMS_FACTOR_TOKEN_DEFAULT;
     }
 
     private SmsFactorFacade createSmsFactorFacade() {
         String serviceUrl = SMS_FACTOR_SERVICE_URL_DEFAULT;
+
+        final String envVar = System.getenv(SMS_FACTOR_SERVICE_URL_ENV);
+        if (StringUtil.isNotBlank(envVar)) {
+            serviceUrl = envVar;
+        }
+
         if (componentModel.isPresent()) {
             serviceUrl = componentModel.get().get(SMS_FACTOR_SERVICE_URL_CONFIG);
         }
+
         return new SmsFactorFacade(session.getProvider(HttpClientProvider.class).getHttpClient(), serviceUrl, getSmsFactorToken());
     }
 }
